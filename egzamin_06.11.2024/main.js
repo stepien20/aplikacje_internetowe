c46971c3b89957fad61ebeef7ca7ac9363aba38e
stepien20 <102522088+stepien20@users.noreply.github.com>
2024-11-06 12:17:05 +0100

    Add files via upload

---
+++ egzamin_06.11.2024/main.js
@@ -0,0 +1,11 @@
+const pszczola_but=document.querySelector(`#pszczola_but`);
+const pomarancza_but_color=document.querySelector(`#pomarancza_but_color`);
+const pomarancza_but_grayscale=document.querySelector(`#pomarancza_but_grayscale`);
+const owoce_but=document.querySelector(`#owoce_but`)
+const zolw_but=document.querySelector(`#zolw_but`)
+
+pszczola_but.addEventListener(`click`,()=>{
+
+
+})
+
